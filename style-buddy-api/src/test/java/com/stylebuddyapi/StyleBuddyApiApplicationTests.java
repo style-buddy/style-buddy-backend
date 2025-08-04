1851facb69e1840144378d675c02a1070c510ebf
Seungwon-Choi <bestwondev@gmail.com>
2025-08-04 15:40:04 +0900

    chore: initialize spring boot #1

---
+++ style-buddy-api/src/test/java/com/stylebuddyapi/StyleBuddyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stylebuddyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyleBuddyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
